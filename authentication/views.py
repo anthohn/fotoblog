f2cd8f892183fddc55bbf9769316ece01be3a773
Anthony Höhn <anthonyhohn@gmail.com>
2023-11-29 15:13:45 +0100

    upload, show, update profile pic

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
-
+from django.contrib.auth.decorators import login_required
 from . import forms
 
 def signup_page(request):
@@ -14,3 +14,13 @@ def signup_page(request):
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
     return render(request, 'authentication/signup.html', context={'form': form})
+
+@login_required
+def upload_profile_photo(request):
+    form = forms.UploadProfilePhotoForm(instance=request.user)
+    if request.method == 'POST':
+        form = forms.UploadProfilePhotoForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'authentication/upload_profile_photo.html', context={'form': form})
--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('home/', blog.views.home, name='home'),
     path('signup/', authentication.views.signup_page, name='signup'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
+    path('profil/photo/upload/', authentication.views.upload_profile_photo, name='upload_profile_photo'),
 ]
 # Condition pour vérifier si le mode DEBUG est activé dans les paramètres du projet
 if settings.DEBUG:
