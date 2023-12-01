662d96d894dd2c0ec25fabfe4f1cc39accf3c55f
Anthony Höhn <anthonyhohn@gmail.com>
2023-12-01 11:41:43 +0100

    pagination

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.forms import formset_factory
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
 
 from . import forms, models
 
@@ -37,9 +38,11 @@ def home(request):
         reverse=True
     )
 
-    context = {
-        'blogs_and_photos': blogs_and_photos,
-    }
+    paginator = Paginator(blogs_and_photos, 6)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'blog/home.html', context=context)
 
 
--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -1,47 +1,42 @@
-"""
-URL configuration for fotoblog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-""" 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 
 import authentication.views
 import blog.views
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LoginView.as_view(template_name='authentication/login.html', redirect_authenticated_user=True), name='login'),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True),
+         name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('change-password/', PasswordChangeView.as_view(template_name='authentication/password_change_form.html'), name='password_change'),
-    path('change-password-done/', PasswordChangeDoneView.as_view(template_name='authentication/password_change_done.html'),name='password_change_done'),
-    path('home/', blog.views.home, name='home'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change_form.html'),
+         name='password_change'
+         ),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+         name='password_change_done'
+         ),
     path('signup/', authentication.views.signup_page, name='signup'),
+    path('profile-photo/upload', authentication.views.upload_profile_photo,
+         name='upload_profile_photo'),
+    path('home/', blog.views.home, name='home'),
+    path('photo-feed/', blog.views.photo_feed, name='photo_feed'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
-    path('profil/photo/upload/', authentication.views.upload_profile_photo, name='upload_profile_photo'),
     path('blog/create', blog.views.blog_and_photo_upload, name='blog_create'),
     path('blog/<int:blog_id>', blog.views.view_blog, name='view_blog'),
     path('blog/<int:blog_id>/edit', blog.views.edit_blog, name='edit_blog'),
-    path('photo/upload-multiple/', blog.views.upload_multiple_photos, name='upload_multiple_photos'),
+    path('photo/upload-multiple/', blog.views.upload_multiple_photos,
+         name='upload_multiple_photos'),
     path('follow-users/', blog.views.follow_users, name='follow_users')
 ]
-# Condition pour vérifier si le mode DEBUG est activé dans les paramètres du projet
+
 if settings.DEBUG:
-    # Si le mode DEBUG est activé, on ajoute une URL pour servir les fichiers media
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
